35f69b5012939afda8e1d0bf84dd7987e4cb0c15
Alexsander Ferreira <automacao.desenvolvimento@gmail.com>
2020-01-28 09:34:06 -0300

    init commit

---
+++ NovoProjeto/src/components/Author.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image} from 'react-native'
+
+export default props => {
+    return (
+        <View style={styles.container}>              
+            <Image source={require('../../assets/imgs/fence.jpg')} style={styles.avatar}></Image>
+            <Text style={styles.nickname}>{props.nickname}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        marginHorizontal: 10
+    },
+    nickname: {
+        color: '#444',
+        marginVertical: 10,
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+})
